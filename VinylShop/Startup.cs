5626968502d056fb2b0428b7e48275060a9c602e
Giorgos Nerantzis <infidel6@hotmail.com>
2019-05-31 14:02:57 +0300

    Add project files.

---
+++ VinylShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VinylShop.Startup))]
+namespace VinylShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
