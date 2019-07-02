fd981c4fcc166294b806911ad09ea4428f3c72f6
Giorgos Nerantzis <infidel6@hotmail.com>
2019-07-02 15:47:18 +0300

    added MyCart in menu and Controller additions

--- VinylShop/Controllers/myCartController.cs
+++ VinylShop/Controllers/myCartController.cs
@@ -3,15 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VinylShop.Models;
 
 namespace VinylShop.Controllers
 {
     public class myCartController : Controller
     {
+
+        private VinylShopDBContext db = new VinylShopDBContext();
+
+
         // GET: myCart
         public ActionResult Index()
         {
-            return View();
+            Customer customer = db.Customers.Find(1);
+
+            return View(customer);
+        }
+
+        public ActionResult DeleteFromCart(int? customerId, int vinylId)
+        {
+            var customer = db.Customers.Find(customerId);
+
+            var temp = customer.Vinyls.Where(v => v.ID == vinylId).ToList();
+
+            foreach (var item in temp)
+            {
+                if (item.ID == vinylId)
+                {
+                    customer.Vinyls.Remove(item);
+                }
+            }
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
